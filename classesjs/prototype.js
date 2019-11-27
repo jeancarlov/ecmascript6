2a1a862ae304a16b43c59646667bad15cf8bea78
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-27 10:49:00 -0500

    prototypes

--- classesjs/prototype.js
+++ classesjs/prototype.js
@@ -6,6 +6,24 @@ function wizard (name, house, pet){
     this.greet = () => `I'm ${this.name} from ${this.house}`
 }
 
-let harry = new wizard('harry', 'grif', 'owl')
+//Change the name pet name from the prototype
+wizard.prototype.pet_name;
+
+//Info want is giving an undifined result because arrow function works when the method is define in the prototype scope 
+// Therefor the solution is to use a function declaration in info2
+wizard.prototype.info1 = () => {
+    return ` I have a ${this.pet} named ${this.pet_name}`
+}  //I have a undefined named undefined
+
+wizard.prototype.info2 = function () {
+    return ` I have a ${this.pet} named ${this.pet_name}`
+} // I have a owl named hedwig
+
+let harry = new wizard('harry', 'grif', 'owl');
+harry.pet_name= "hedwig";
+console.log(harry);
 console.log(harry.greet());
+console.log(harry.info1()); // I have a undefined named undefined
+console.log(harry.info2());  // I have a owl named hedwig
+
 
