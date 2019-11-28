32ebc95c50cef034662f81ce39b6dba32ef394d6
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-27 19:40:29 -0500

    closures

---
+++ closures/closures.js
@@ -0,0 +1,20 @@
+let call = () => {
+    let secrect = "es6 rocks!";
+}
+
+// console.log(secrect);// secrect is denied in the global scope
+
+// solution is to use a closure to gain acces to this data with a closure method that reveals data.
+// use reveal method from closures to get the secrect varible
+
+let call1 = () => {
+    let secrect1 = "es6 rocks for real!";
+    let reveal = () => {
+        console.log(secrect1);
+        
+    }
+    reveal();
+}
+
+call1();
+
