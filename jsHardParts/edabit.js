eac6bedc582857f66afcdbc82d5032f662c85367
Jean Valderrama <jeanvalderrama@jeans-mbp-3.home>
2020-11-27 20:08:13 -0500

    more code challenges

--- jsHardParts/edabit.js
+++ jsHardParts/edabit.js
@@ -1,17 +1,133 @@
 
+// function hello(x) {
+//   if (x === x) {
+//     return "hello edabit.com";
+//   }
+// }
 
-function howManySeconds(x) {
-  return x * 3600;
-}
+// let result = hello("x");
+// console.log(result);
 
-let result = howManySeconds(2);
-console.log(result);
+// function Hello() {
+//   return "hello edabit.com";
+// }
+
+// let result1 = Hello("x");
+// console.log(result1);
+
+//-------------------------------
+
+// function howManySeconds(x) {
+//   return x * 3600;
+// }
+
+// let result = howManySeconds(2);
+// console.log(result);
 
 
-function howManySeconds2(hours) {
-  const mins = 60;
-  const seconds = 60;
-  return hours * mins * seconds;
-}
+// function howManySeconds2(hours) {
+//   const mins = 60;
+//   const seconds = 60;
+//   return hours * mins * seconds;
+// }
 
-let howManySeconds3 = (h) => h * 3600;
+// let howManySeconds3 = (h) => h * 3600;
+
+//-------------------------------
+
+// function circuitPower(voltage, current) {
+//     return voltage * current;
+// }
+
+// let result3 = circuitPower(230,10)
+// console.log(result3);
+
+// const circuitPowerArrowFn = (voltage, current) => voltage * current;
+
+//-------------------------------
+
+// function getFirstValue(arr) {
+//  return arr[0];
+// }
+
+
+// let result4 =getFirstValue([1, 2, 3]) 
+// console.log(result4);
+
+// const getFirstValueArroFn = (arr) => arr[0];
+
+// function remainder(x, y) { 
+//     return x % y
+// }
+
+// let result6 = remainder(15,7)
+// console.log(result6);
+
+//-------------------------------
+
+// function triArea(base, height) {
+
+//     return (base * height) /2 
+// }
+
+// let result7 =triArea(3,2)
+// console.log(result7);
+
+// function triArea(base, height) {
+//   var a = base * height;
+//   var b = a / 2;
+//   return b;
+// }
+
+// const triArea = (base, height) => (base * height) / 2;
+
+//-------------------------------
+
+// function lessThan100(a, b) {
+//     let total = a + b;
+//     if (total < 100) {
+//         return true
+//     } else if (total >100) {
+//         return false
+//     } else return true
+// }
+
+// let result8 = lessThan100(3, 77);
+// console.log(result8);
+
+// function lessThan100(a, b) {
+//   return a + b < 100 ? true : false;
+// }
+
+// function lessThan100(a, b) {
+//   return a + b < 100;
+// }
+
+// let result8 = lessThan100(3, 77);
+// console.log(result8);
+
+// -------------------------
+
+// function addition(num) {
+
+//     return ++num;
+// }
+
+// let result10 = addition(1)
+// console.log(result10);
+
+// const addition = (num) => num + 1;
+
+
+// function cube(x) {
+//     return x * x * x
+// }
+
+// let result11 =cube(5);
+// console.log(result11);
+
+// function cubes(a) {
+//   return a ** 3;
+// }
+
+// let result = cubes(3);
