06cbafe220c746eaa6aa3efbbb8dc4565f20a9ba
Jean Valderrama <jeanvalderrama@jeans-mbp-3.home>
2020-11-30 18:01:21 -0500

    more logic code challenges

--- jsHardParts/medium.js
+++ jsHardParts/medium.js
@@ -14,7 +14,7 @@
 
 // let result1= convert(1,3)
 // console.log(result1);
-
+//---------------------------------
 // function and(a, b) {
 //   if (a && b) {
 //     return true;
@@ -40,7 +40,7 @@
 //   let bool = a === b && b != false ? true : false;
 //   return bool;
 // }
-
+//---------------------------------
 // function findPerimeter(length, width) {
 //     let firstSide= length *2;
 //     let secondSide= width *2;
@@ -55,7 +55,7 @@
 // }
 
 // const findPerimeter = (h, w) => (w + h) * 2;
-
+//---------------------------------
 // function maxNum(n1, n2) {
 //   if (n1 < n2) {
 //     return n2;
@@ -69,13 +69,97 @@
 
 // const maxNum = (n1, n2) => (n1 < n2 ? n2 : n1);
 
+//---------------------------------
+// function giveMeSomething(a) {
+//     let newPhrase= `something ${a}`;
+//     return newPhrase
+// }
 
-function giveMeSomething(a) {
-    let newPhrase= `something ${a}`;
-    return newPhrase
+// let result4 = giveMeSomething("is better than nothing");
+// console.log(result4);
+
+// const giveMeSomethingArrowFn = (a) => `something ${a}`;
+
+//---------------------------------
+// function matchHouses(step) {
+// if (step) {
+// 		return (step * 5) + 1;
+// 	}
+// 	return 0;
+// }
+
+
+// function binary(decimal) {
+//   let result = "";
+//   if (decimal == 0) {
+//     result = "0";
+//   }
+//   while (decimal > 0) {
+//     result += decimal % 2;
+//     decimal = Math.floor(decimal / 2);
+//   }
+//   return result.split("").reverse().join("");
+// }
+
+
+// function binary(decimal) {
+//   return decimal.toString(2);
+// }
+// let result5 =binary(5)
+// console.log(result5);
+
+//-------------
+
+
+// function minMax(arr) {
+//   var returnArr = [];
+//   var min = arr[0];
+//   var max = arr[0];
+
+//   for (var i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+
+//   returnArr.push(min);
+//   returnArr.push(max);
+
+//   return returnArr;
+// }
+
+// let resultMinMAx= minMax([1, 2, 3, 4, 5]);
+// console.log(resultMinMAx);
+
+// function minMaxBetter(arr) {
+//   return [Math.min(...arr), Math.max(...arr)];
+// }
+// let resultMinMAxBetter = minMax([2334454, 5]);
+// console.log(resultMinMAxBetter);
+
+// const minMaxArrowFn = (arr) => [Math.min(...arr), Math.max(...arr)];
+
+// function whichIsLarger1(f, g) {
+//   if (f() < g()) return "g";
+//   else if (f() > g()) return "f";
+//   else return "neither";
+// }
+
+// function whichIsLarger2(f, g) {
+//   return f() > g() ? "f" : f() < g() ? "g" : "neither";
+// }
+
+//-------------
+function addUp(num) {
+let array = [];
+for (let i = 0; i <= num; i++) {
+  array.push(i);
+}
+return array.reduce((a, b) => a + b);
 }
 
-let result4 = giveMeSomething("is better than nothing");
-console.log(result4);
-
-const giveMeSomethingArrowFn = (a) => `something ${a}`;
+let result6 = addUp(4);
+console.log(result6);
